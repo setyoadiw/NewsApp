310655f98b1e3a6967460282751a968a5036818f
Setyo Adi Wibowo <setyaadii12@gmail.com>
2019-04-20 15:25:04 +0700

    add adapter

--- app/src/main/java/com/setyo/newsapplication/screen/main/MainActivity.kt
+++ app/src/main/java/com/setyo/newsapplication/screen/main/MainActivity.kt
@@ -2,13 +2,22 @@ package com.setyo.newsapplication.screen.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.widget.Toast
 import com.setyo.newsapplication.R
 import com.setyo.newsapplication.model.ArticlesItem
+import org.jetbrains.anko.find
+
 
 class MainActivity : AppCompatActivity(),MainView {
 
     lateinit var presenter : NewsPresenter
+
+    //recyclerview
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: MainAdapter
+    private var data: MutableList<ArticlesItem> = mutableListOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +29,18 @@ class MainActivity : AppCompatActivity(),MainView {
     }
 
     private fun initView() {
+        //recyclerView
+        recyclerView = find(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MainAdapter(this, data, { partItem: ArticlesItem -> partItemClicked(partItem) })
+        recyclerView.adapter = adapter
+
         presenter.getDataFromApi()
+    }
+
+    private fun partItemClicked(partItem: ArticlesItem) {
+        Toast.makeText(this,partItem.description,Toast.LENGTH_LONG).show()
+
     }
 
     private fun initPresenter() {
@@ -29,6 +49,11 @@ class MainActivity : AppCompatActivity(),MainView {
 
     override fun onSuccess(data: List<ArticlesItem>) {
         Toast.makeText(this,data.get(0).author.toString(),Toast.LENGTH_LONG).show()
+
+        //set to adapter
+        this.data.clear()
+        this.data.addAll(data)
+        adapter.notifyDataSetChanged()
 
     }
 
